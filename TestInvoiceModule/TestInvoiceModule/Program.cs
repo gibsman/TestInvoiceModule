409c761e478f8c8ae476f2871883e0dba38c4ff1
gibsman <gibsman319@gmail.com>
2020-11-14 21:32:56 +0600

    Add Initial files

---
+++ TestInvoiceModule/TestInvoiceModule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestInvoiceModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
