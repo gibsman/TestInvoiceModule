55c2dcdf56ea9982362c09a40ad33f2b67c640fb
gibsman <gibsman319@gmail.com>
2020-11-15 03:52:56 +0600

    Add conversion of invoice template from docs to pdf

--- TestInvoiceModule/TestInvoiceModule/Program.cs
+++ TestInvoiceModule/TestInvoiceModule/Program.cs
@@ -4,6 +4,8 @@ using MimeKit;
 using System.IO;
 using MailKit.Net.Smtp;
 using MailKit.Security;
+using Spire.Doc;
+using Spire.Doc.Documents;
 
 namespace TestInvoiceModule
 {
@@ -12,15 +14,33 @@ namespace TestInvoiceModule
         static void Main(string[] args)
         {
             SmtpClient client = InitializeClient();
-            for (int i = 0; i < 10; i++)
+            Document invoiceDoc = InitializeInvoice();
+            for (int id = 0; id < 10; id++)
             {
+                ConvertDocToPdf(invoiceDoc, id.ToString());
                 var watch = System.Diagnostics.Stopwatch.StartNew();
-                SendMail("starkovmaximdev@gmail.com", client);
+                string pdfName = id + ".pdf";
+                SendMail(pdfName, ConfigurationManager.AppSettings["TestMail"], client);
                 watch.Stop();
                 Console.WriteLine((double)watch.ElapsedMilliseconds / 1000);
+                File.Delete(pdfName);
             }
             client.Disconnect(true);
             Console.WriteLine("DONE");
+        }
+
+        private static Document InitializeInvoice()
+        {
+            Document document = new Document();
+            document.LoadFromFile("invoice_template.docx");
+            return document;
+        }
+
+        private static void ConvertDocToPdf(Document originalInvoice, string id)
+        {
+            Document invoiceCopy = originalInvoice.Clone();
+            invoiceCopy.Replace("order_id", id, true, true);
+            invoiceCopy.SaveToFile(id + ".PDF", FileFormat.PDF);
         }
 
         private static SmtpClient InitializeClient()
@@ -32,19 +52,18 @@ namespace TestInvoiceModule
             return client;
         }
 
-        private static void SendMail(string recipientMail, SmtpClient client)
+        private static void SendMail(string pdfPath, string recipientMail, SmtpClient client)
         {
             var mailMessage = new MimeMessage();
-            mailMessage.From.Add(new MailboxAddress("Starkov Maxim", ConfigurationManager.AppSettings["UserMail"]));
+            mailMessage.From.Add(new MailboxAddress("Placeholder Company", ConfigurationManager.AppSettings["UserMail"]));
             mailMessage.To.Add(new MailboxAddress("Placeholder Guy", recipientMail));
-            mailMessage.Subject = "Test Message";
-            string path = AppDomain.CurrentDomain.BaseDirectory + "blank.pdf";
+            mailMessage.Subject = "Invoce For Order";
             var attachment = new MimePart("application", "pdf")
             {
-                Content = new MimeContent(File.OpenRead(path), ContentEncoding.Default),
+                Content = new MimeContent(File.OpenRead(pdfPath), ContentEncoding.Default),
                 ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
                 ContentTransferEncoding = ContentEncoding.Base64,
-                FileName = Path.GetFileName(path)
+                FileName = Path.GetFileName(pdfPath)
             };
             var multipart = new Multipart("mixed");
             multipart.Add(attachment);
