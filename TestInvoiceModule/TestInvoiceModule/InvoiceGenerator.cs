dd0605cc8f163b268aeeadc18ca7169e0c811036
gibsman <gibsman319@gmail.com>
2020-11-17 00:14:21 +0600

    Add invoice generator class with method for creating basic template

---
+++ TestInvoiceModule/TestInvoiceModule/InvoiceGenerator.cs
@@ -0,0 +1,29 @@
+﻿using MigraDoc.DocumentObjectModel;
+using MigraDoc.DocumentObjectModel.Tables;
+using MigraDoc.Rendering;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestInvoiceModule
+{
+    public class InvoiceGenerator
+    {
+        Document templateDoc;
+
+        public InvoiceGenerator()
+        {
+            CreateTemplate();
+        }
+
+        private void CreateTemplate()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            templateDoc = new Document();
+            templateDoc.Info.Subject = "Invoice for several items purchased from Placeholder Company";
+            templateDoc.Info.Author = "Placeholder Company";
+        }
+    }
+}
