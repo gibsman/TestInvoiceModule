ba6af02703f909957afb9308c015fcbb13aa8f85
gibsman <gibsman319@gmail.com>
2020-11-21 01:19:40 +0600

    Add OrderProcessor class

---
+++ TestInvoiceModule/TestInvoiceModule/Logic/OrderProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInvoiceModule
+{
+    public class OrderProcessor
+    {
+        List<Order> generatedOrders;
+
+        public OrderProcessor(int orderCount)
+        {
+            TestData data = new TestData();
+            generatedOrders = data.GenerateRandomTestOrders(orderCount);
+        }
+
+        //returns time spent on generating invoices (in seconds)
+        public double GenerateInvoices()
+        {
+            var watch = Stopwatch.StartNew();
+            //generates all PDF invoices concurrently
+            Parallel.ForEach(generatedOrders, order => InvoiceGenerator.Generate(order));
+            watch.Stop();
+            double pdfGenerationTime = (double)watch.ElapsedMilliseconds / 1000;
+            return pdfGenerationTime;
+        }
+
+        //returns time spent on sending invoices (in seconds)
+        public double SendInvoices()
+        {
+            var watch = Stopwatch.StartNew();
+            watch.Start();
+            MailManager.SendMailBatch(generatedOrders);
+            watch.Stop();
+            double mailSentTime = (double)watch.ElapsedMilliseconds / 1000;
+            return mailSentTime;
+        }
+    }
+}
