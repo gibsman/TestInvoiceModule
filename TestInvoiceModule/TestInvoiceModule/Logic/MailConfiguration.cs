7ea998bd43c93c3882e9dd4f8dafd03d088f4a65
gibsman <gibsman319@gmail.com>
2020-11-27 17:36:56 +0600

    Add IMailConfiguration interface with its implementaion

---
+++ TestInvoiceModule/TestInvoiceModule/Logic/MailConfiguration.cs
@@ -0,0 +1,26 @@
+﻿namespace TestInvoiceModule
+{
+    public interface IMailConfiguration
+    {
+        string ClientMail { get; set; }
+
+        string SmtpUserName { get; set; }
+
+        string SmtpPassword { get; set; }
+
+    }
+
+    class MailConfiguration : IMailConfiguration
+    {
+        public string ClientMail { get; set; }
+
+        public string SmtpUserName { get; set; }
+
+        public string SmtpPassword { get; set; }
+
+        public MailConfiguration()
+        {
+
+        }
+    }
+}
